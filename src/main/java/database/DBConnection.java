da67f306b6a71a7a1e4436fb65b37b2f8fd7d11b
hs986 <94986211+hs986@users.noreply.github.com>
2024-05-07 18:25:55 +0900

    db connection 5/7

--- src/main/java/database/DBConnection.java
+++ src/main/java/database/DBConnection.java
@@ -1,5 +1,30 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+
+
 public class DBConnection {
+	private Connection con; //커넥션 객체
+	private Statement st; //statementc 특정 db sql 문장을 실행하는 방법
+	private ResultSet rs; //결과를 받아오는 객체
+   
+	public DBConnection() {
+		try
+		{
+	         Class.forName("com.mysql.jdbc.Driver");
+	         con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/AI_grade","root","1234");
+	         st =con.createStatement();
+	    }
+	    catch(Exception e)
+	    {
+	    	System.out.println("데이터베이스 연결 오류"+ e.getMessage());
+	    }
+	}
+
 
 }
