62475bcdb7159e36f3a839f81a960f516bdccc29
kny5579 <kny5579@naver.com>
2024-11-05 21:45:10 +0900

    Revert "result페이지 수정 중"
    This reverts commit 0767d6058b735aa11cb8753f30becb0e84d1a6e8.

--- src/main/java/org/example/smartScore/repository/ExcelFileRepository.java
+++ src/main/java/org/example/smartScore/repository/ExcelFileRepository.java
@@ -2,7 +2,6 @@ package org.example.smartScore.repository;
 
 import org.example.smartScore.domain.ExcelFile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Date;
 import java.util.List;
@@ -10,7 +9,4 @@ import java.util.Optional;
 
 public interface ExcelFileRepository extends JpaRepository<ExcelFile, Long> {
     List<ExcelFile> findByDate(Date date);
-
-    @Query("SELECT MAX(e.date) FROM excel_file e")
-    Optional<Date> findLatestDate();
 }
