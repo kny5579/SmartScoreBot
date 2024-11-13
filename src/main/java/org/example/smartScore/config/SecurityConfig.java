3171eda01d92d9e89103669ed61828c243c928b7
hs986 <94986211+hs986@users.noreply.github.com>
2024-11-13 14:37:30 +0900

    비번메세지 오류 해결!!!

--- src/main/java/org/example/smartScore/config/SecurityConfig.java
+++ src/main/java/org/example/smartScore/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 .authorizeRequests() // 인증, 인가 설정
                 .requestMatchers("/images/**").permitAll()
                 .requestMatchers("/", "/index", "/login", "/signup", "/user", "/mail/**", "/forgot-password",
-                        "/password-reset-mail", "/verify-password-reset-code").permitAll() //비번 경로 추가
+                        "/password-reset-mail", "/verify-password-reset-code","/reset-password").permitAll() //비번 경로 추가
                 .anyRequest().authenticated()
                 .and()
                 .formLogin() // 폼 기반 로그인 설정
